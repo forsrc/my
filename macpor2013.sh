3f2ac3045ab6424df177cea151057db8cf405cf9
forsrc <forsrc@gmail.com>
2024-07-21 23:03:08 +0900

    Create macpor2013.sh

---
+++ macpor2013.sh
@@ -0,0 +1,6 @@
+
+# ubuntu 18.04
+wget --referer support.amd.com https://drivers.amd.com/drivers/linux/amdgpu-pro-18.50-725072-ubuntu-18.04.tar.xz 
+tar xf amdgpu-pro-18.50-725072-ubuntu-18.04.tar.xz
+cd amdgpu-pro-18.50-725072-ubuntu-18.04
+./amdgpu-pro-install 
